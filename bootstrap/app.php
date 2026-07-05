a2cb4a413105fc09668a608fa9066ef15b7c1f28
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2026-07-05 13:43:00 +0100

    Prepare SQLite deployment without shell access

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+$sqlitePath = dirname(__DIR__).'/database/database.sqlite';
+
+if (! file_exists($sqlitePath)) {
+    if (! is_dir(dirname($sqlitePath))) {
+        mkdir(dirname($sqlitePath), 0755, true);
+    }
+
+    touch($sqlitePath);
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
